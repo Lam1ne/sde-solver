3b237791aa06057a46388c1def69f62d735abfa0
Lamine <31112349+Lam1ne@users.noreply.github.com>
2026-03-18 13:35:56 +0100

    Update euler_maruyama.py

--- src/sde_solver/euler_maruyama.py
+++ src/sde_solver/euler_maruyama.py
@@ -12,17 +12,13 @@ where:
 
 import numpy as np
 
-# TODO: maybe add Milstein scheme later for better convergence?
 DEBUG = False  # quick flag for printing stuff while debugging
 
 
 def euler_maruyama(X0, a, b, T, N, M=1):
     """
     Euler-Maruyama scheme to solve an SDE numerically
-
-    This is the main function I implemented for my project.
-    It's basically the stochastic version of Euler's method.
-
+    
     Parameters:
     X0 - initial condition
     a - drift function a(X, t)
@@ -40,13 +36,10 @@ def euler_maruyama(X0, a, b, T, N, M=1):
     X = np.zeros((M, N + 1))
     X[:, 0] = X0
 
-    # Generate Brownian increments - this is the random part!
     dW = np.sqrt(dt) * np.random.randn(M, N)
 
     # Main loop
     for i in range(N):
         X[:, i + 1] = X[:, i] + a(X[:, i], t[i]) * dt + b(X[:, i], t[i]) * dW[:, i]
-        # if DEBUG and (i % 200 == 0 or i == N-1):
-        #     print('step', i, 'mean(X)=', float(X[:, i].mean()))
 
     return t, X
